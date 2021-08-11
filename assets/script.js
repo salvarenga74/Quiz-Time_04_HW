e8a03949ae48166c7a60a9c31ba08d0cc3f22c40
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-08-10 23:44:24 -0500

    After working with my tutor I sucessfully set display attributes for the different sections and I created a fully working structure of the rotating question elements that will be used going foward.

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,164 @@
 var startingPageEl = document.querySelector("#startingPage");
 var cardDeckEl = document.querySelector("#cardDeck");
 var summaryPageEl = document.querySelector("#summaryPage");
+var startButton = document.querySelector(".startButton");
+var timeRemaining = document.querySelector(".timeLeft");
 
-// sendToStartingPage();
 
-// function sendToStartingPage(){
-//     cardDeckEl.textContent= " ";
-//     startingPageEl.bigMainText.textContent= "Start Game";
-   
-   
-// }
-// sendToCardDeck();
-// setTime();
-// function sendToCardDeck(){
-//     startingPageEl.textContent= " ";
-//     summaryPageEl = " ";
-// }
+var countDownTime = 4;
+var userPointsScored = 0;
+var individualScoreList = [];
+var questionCounter = 0;
+var answerButtons = document.querySelectorAll(".list-group-item-action");
+var currentQuestionText = document.querySelector(".currentQuestion");
 
-// function sendToSummary() {
-//     cardDeckEl.textContent= " ";
+for( var i=0; i< answerButtons.length; i++){
+    var button= answerButtons[i];
+    button.addEventListener("click",submitAnswer);
+}
 
-//     summaryPageEl.textContent = "GameEnd"
-
-// }
-
-// function setTime() {
-//     let countDownTime = 4;
-//     startingPageEl= " ";
-//     cardDeckEl.textContent= " ";
+// 
+// Questions to be Asked
+// 
+var questionArray = [
+    {
+        question: 'What color is the sky?', 
+        correctAnswer: 'blue',
+        answers: ['red','blue', 'green', 'purple']
+    }
     
-//     let timerInterval = setInterval(function(){
-//         countDownTime--;
-//         cardDeckEl.textContent = countDownTime + " Time Remaining";
+];
+
+function submitAnswer() {
+   console.log("I want to see the function being called");
+    questionCounter++;
+//    want to check if the answer is correct,
+//  and if its correct increse score
+// and if not reduce the time
+// and call the thing that will replace all the text content
+}
+
+function getQuestion(){
+    var currentQuestion = questionArray[questionCounter];
+    currentQuestionText.textContent= currentQuestion.question;
+
+    for( var i=0; i< currentQuestion.answers.length; i++){
+        var button= answerButtons[i];
+        var answer = currentQuestion.answers[i];
+        button.textContent= answer;
+    }
+}
+
+// Calls init() so that it fires when page opened
+init();
+// Attach event listener to start button to call the sendToCardDeck Section on click
+startButton.addEventListener("click", sendToCardDeck);
+
+
+function init() {
+    pointsScored();
+    sendToStartingPage();
+    
+    var storedScores= JSON.parse(localStorage.getItem("individualScoreList"));
+    
+    // If individualScoreList were retrieved from localStorage, update the individualScoreList to it
+    if (storedScores !== null) {
+        individualScoreList = storedScores;
+    }
+    
+    // This is a helper function that will render todos to the DOM
+    renderPreviousScoreList();
+}
+
+function sendToStartingPage(){
+    //    cardDeckEl.style.display = "none";
+    //    summaryPageEl.style.display = "none";
+}
+
+function sendToCardDeck(){
+    startingPageEl.setAttribute("class", "hide");
+    cardDeckEl.removeAttribute("class");
+    setTime();
+
+    getQuestion();
+}
+
+function sendToSummary() {
+    cardDeckEl.setAttribute("class", "hide");
+    summaryPageEl.removeAttribute("class");
+    
+    
+}
+
+
+
+// 
+// Score Card Section
+// 
+var userInitialInput = document.querySelector("#initialsText");
+var initialForm = document.querySelector("#initialForm");
+
+
+
+
+function pointsScored(){
+    // if (questionEvaluation===true)
+    userPointsScored++;
+    console.log(userPointsScored);
+}
+
+function storingScores() {
+    // Stringify and set key in localStorage to individualScoreList array
+    localStorage.setItem("individualScoreList", JSON.stringify(individualScoreList));
+  }
+
+function renderPreviousScoreList(){
+    var highScoreList = document.querySelector("#highScoreList");
+    highScoreList.textContent = " ";
+
+     // Render a new li for each todo
+     for (var i = 0; i < individualScoreList.length; i++) {
+        var name= individualScoreList[i];
+    
+        var li = document.createElement("li");
+        li.textContent = name;
+        li.setAttribute("data-index", i);
+    
+        highScoreList.appendChild(li);
+      }
+}
+
+// Add submit event to Initials form
+initialForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    var textForInitialsList = userInitialInput.value.trim();
+  
+    // Return from function early if submitted textForInitialsList is blank
+    if (textForInitialsList === "") {
+      return;
+    }
+  
+    // Add new textForInitialsList to todos array, clear the input
+    individualScoreList.push(textForInitialsList);
+    userInitialInput.value = " ";
+  
+    // Store updated todos in localStorage, re-render the list
+    storingScores();
+    renderPreviousScoreList();
+  });
+
+// 
+// Timer Function
+// 
+function setTime() {
+    var timerInterval = setInterval(function(){
+        countDownTime--;
+        timeRemaining.textContent = "Time Remaining:"+ countDownTime;
         
-//         if(countDownTime===0){
-//             clearInterval(timerInterval);
-//             sendToSummary();
-//         }
-//     },1000);
-    
-// }
+        if(countDownTime===0){
+            clearInterval(timerInterval);
+            sendToSummary();
+        }
+    },1000);
+}
