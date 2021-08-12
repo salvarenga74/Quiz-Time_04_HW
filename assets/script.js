71897f40e5ca8ca718c6532f7d5e5becade2861b
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-08-12 01:32:16 -0500

    I have made some minor changes to the appearence and the organization of the page and the codes

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,26 @@
+// 
+// Variables for Starting Page/ initial page
 var startingPageEl = document.querySelector("#startingPage");
-var cardDeckEl = document.querySelector("#cardDeck");
-var summaryPageEl = document.querySelector("#summaryPage");
 var startButton = document.querySelector(".startButton");
-var timeRemaining = document.querySelector(".timeLeft");
-
-
-var countDownTime = 10;
+var countDownTime = 20;
+var questionCounter = 0;
 var userPointsScored = 0;
 var individualScoreList = [];
-var questionCounter = 0;
+
+// 
+// Variables for Summary Page
+var summaryPageEl = document.querySelector("#summaryPage");
+var initialForm = document.querySelector("#initialForm");
+var userInitialInput = document.querySelector("#initialsText");
+var userScoreDisplay = document.querySelector(".scorePointsDisplay");
+
+// 
+// Variables for Question Deck
+var cardDeckEl = document.querySelector("#cardDeck");
+var timeRemaining = document.querySelector(".timeLeft");
 var currentQuestionText = document.querySelector(".currentQuestion");
 var answerButtons = document.querySelectorAll(".list-group-item-action");
-
+var currentQuestion = questionArray[questionCounter];
 var questionArray = [
     {
         question: 'What color is the sky?', 
@@ -31,48 +40,17 @@ var questionArray = [
 
 ];
 
+// 
+// Calls init() so that it fires when page opened
+init();
+
+// Attach event listener to start button to call the sendToCardDeck Section on click
+startButton.addEventListener("click", sendToCardDeck);
+
 for( var i=0; i< answerButtons.length; i++){
     var button= answerButtons[i];
     button.addEventListener("click",submitAnswer);
 }
-
-function submitAnswer(event) {
-    questionCounter++;
-
-    var userPick = event.target.textContent.trim();
-    console.log("this is the user's pick: " + userPick);
-        if(userPick===currentQuestion.correctAnswer){
-        userPointsScored++;
-        console.log("Has the user's point score changed: "+ userPointsScored)
-        }
-        else{
-            countDownTime--;
-        }
-
-    getQuestion(); 
-}
-
-function getQuestion(){
-    currentQuestion = questionArray[questionCounter];
-    currentQuestionText.textContent= currentQuestion.question;
-    console.log("I want to see the questionCounter: " + questionCounter);
-
-    for( var i=0; i< currentQuestion.answers.length; i++){
-        var button= answerButtons[i];
-        var answer = currentQuestion.answers[i];
-        button.textContent= answer;
-    }
-}
-var currentQuestion = questionArray[questionCounter];
-
-// and call the thing that will replace all the text content
-
-
-// Calls init() so that it fires when page opened
-init();
-// Attach event listener to start button to call the sendToCardDeck Section on click
-startButton.addEventListener("click", sendToCardDeck);
-
 
 function init() {
     sendToStartingPage();
@@ -90,6 +68,9 @@ function sendToStartingPage(){
     
 }
 
+// 
+// Functions Relating to the Card Deck
+// 
 function sendToCardDeck(){
     startingPageEl.setAttribute("class", "hide");
     cardDeckEl.removeAttribute("class");
@@ -97,19 +78,50 @@ function sendToCardDeck(){
     getQuestion();
 }
 
+// Replaced text in the question and the choices section of the page
+function getQuestion(){
+    currentQuestion = questionArray[questionCounter];
+    currentQuestionText.textContent= currentQuestion.question;
+    console.log("I want to see the questionCounter: " + questionCounter);
+
+    for( var i=0; i< currentQuestion.answers.length; i++){
+        var button= answerButtons[i];
+        var answer = currentQuestion.answers[i];
+        button.textContent= answer;
+    }
+}
+
+// evaluates the user's choice and performs events accordingly
+function submitAnswer(event) {
+    questionCounter++;
+    let decrement=2;
+    var userPick = event.target.textContent.trim();
+    console.log("this is the user's pick: " + userPick);
+        if(userPick===currentQuestion.correctAnswer){
+        userPointsScored++;
+        console.log("Has the user's point score changed: "+ userPointsScored)
+        }
+        else{
+            countDownTime-=decrement;
+        }
+        if(questionCounter>2){
+            sendToSummary();
+            console.log("User reached end of question deck");
+        }
+        else{
+            getQuestion(); 
+        }
+}
+
 function sendToSummary() {
     cardDeckEl.setAttribute("class", "hide");
     summaryPageEl.removeAttribute("class");
-     
+    userScoreDisplay.textContent = userPointsScored;
 }
 
-
 // 
-// Score Card Section
+// Score Card Section Functions
 // 
-var userInitialInput = document.querySelector("#initialsText");
-var initialForm = document.querySelector("#initialForm");
-
 function storingScores() {
     // Stringify and set key in localStorage to individualScoreList array
     localStorage.setItem("individualScoreList", JSON.stringify(individualScoreList));
@@ -131,7 +143,6 @@ function renderPreviousScoreList(){
       }
 }
 
-
 // Add submit event to Initials form
 initialForm.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -143,11 +154,11 @@ initialForm.addEventListener("submit", function(event) {
       return;
     }
   
-    // Add new textForInitialsList to todos array, clear the input
+    // Add new textForInitialsList and score to score board list array, clear the input
     individualScoreList.push(textForInitialsList);
     userInitialInput.value = " ";
   
-    // Store updated todos in localStorage, re-render the list
+    // Store updated scores in localStorage, re-render the list
     storingScores();
     renderPreviousScoreList();
 });
@@ -164,6 +175,7 @@ function setTime() {
         if(countDownTime<0){
             clearInterval(timerInterval);
             sendToSummary();
+            console.log("User has run out of time.")
         }
     },1000);
 }
